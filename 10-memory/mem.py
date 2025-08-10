a2ce92979f4b71cd4e45449ac956b93d8921af95
Shobhit-Real-Gupta <guptashobhit2024@gmail.com>
2025-08-11 01:08:56 +0530

    memory

---
+++ 10-memory/mem.py
@@ -0,0 +1,66 @@
+from mem0 import Memory
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+import json
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
+
+config = {
+    "version": "v1.1",
+    "embedder": {
+        "provider": "openai",
+        "config": {
+            "api_key": OPENAI_API_KEY,
+            "model": "text-embedding-3-small"
+        }
+    },
+    "llm": {
+        "provider": "openai",
+        "config": {
+            "api_key": OPENAI_API_KEY,
+            "model": "gpt-4.1-nano"
+        }
+    },
+    "vector_store": {
+        "provider": "qdrant",
+        "config": {
+            "host": "vector-db",
+            "port": "6333"
+        }
+    }
+}
+
+mem_client = Memory.from_config(config)
+
+
+def chat():
+    while True:
+        user_query = input(">")
+        relevant_memories = mem_client.search(query=user_query, user_id="shobhit")
+        memories = [f"ID: {mem.get("id")}, Memory: {mem.get("memory")}" for mem in relevant_memories.get("results")]
+        SYSTEM_PROMPT = F"""
+        You are an memory aware assistant which resond to user with context.
+        You are given with past memories and facts about the user.
+        Memories: {json.dumps(memories)}
+        """
+        response = client.chat.completions.create(
+            model="gpt-4.1-nano",
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": user_query}]
+        )
+
+        print(f"ai - {response.choices[0].message.content}")
+        mem_client.add(
+            [
+                {"role": "user", "content": user_query},
+                {"role": "assistant", "content": response.choices[0].message.content}
+            ],
+            user_id="shobhit"
+        )
+
+
+chat()
